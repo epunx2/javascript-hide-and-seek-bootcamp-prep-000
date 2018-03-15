df1d326f6eb8102e558af97f03ee84c95b60adc4
Eric Chrobak <eric@ericchrobak.com>
2018-03-15 01:39:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  let node = document.getElementById('grand-node');
+  
+  while (node.children[0]) {
+    node = node.children[0]
+  }
+  return node;
+}
